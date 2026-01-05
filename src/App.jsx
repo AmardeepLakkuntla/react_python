adcb56580af1dc7f1e9ff38984acd7f13f543266
amardeeplakkuntla <lakkuntlaamardeep@gmail.com>
2026-01-05 21:23:50 +0530

    need some working

--- src/App.jsx
+++ src/App.jsx
@@ -2,71 +2,25 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
- 
+import {Routes,Route} from 'react-router-dom';
+import Home from "./pages/Home";
+import Posts from "./pages/Posts"
+import Users from './pages/Users';
+import Todos from './pages/Todos';
+
 function App() {
  
-  const [posts, setposts] = useState([])
- 
-  const [page,setPage] = useState(1);
- 
-  const POSTS_PER_PAGE = 10;
- 
-   const fetchPosts = async () => {
-        const response  = await fetch("https://jsonplaceholder.typicode.com/posts");
-        if (response.ok){
- 
-          const data = await response.json()
-          setposts(data)
-        }
-      }
-  useEffect(()=>{
- 
-    fetchPosts();
- 
-  },{})
- 
-  //calculate slliced pages
- 
-  const startIndex = (page -1) * POSTS_PER_PAGE
-  const stopIndex = startIndex + POSTS_PER_PAGE
- 
-  const visiblePosts = posts.slice(startIndex,stopIndex);
- 
-  return (
-    <>
-      <div class="row">
-        <div class="col-sm-12 mb-3 mb-sm-0">
- 
- 
- 
-          {
- 
-            visiblePosts.map((post) => (
-              <div class="card text-start">
-                <div class="card-body text-start">
-                  <li key={post.id} style={{ listStyleType: 'none'}}>
-                    <h5 class="card-title">{post.id} . {post.title} </h5>
-                    <p class="card-text">{post.body}</p>
-                  </li>
-                </div>  
-              </div>
-            ))
- 
-          }
- 
-           <button onClick={() => setPage(page - 1)}
-            >
-            PREVIOUS
-           </button>
-          <button onClick={() => setPage(page + 1)}
-          >
-            NEXT
-          </button>
- 
-        </div>
-      </div>
-    </>
-  )
+return(
+  <Routes>
+    <Route path="/" element={<Home/>}></Route>
+    <Route path="/Posts" element={<Posts/>}></Route>
+    <Route path="/Users" element={<Users/>}></Route>
+    <Route path="/Todos" element={<Todos/>}></Route>
+
+  </Routes>
+
+)
+  
 }
  
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,50 @@
+
+import { useNavigate } from "react-router-dom";
+ 
+function Home() {
+  const navigate = useNavigate();
+ 
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Home</h2>
+ 
+      <button onClick={() => navigate("/posts")}>Posts</button>
+      {/* <br /> <br/> */}
+ 
+      <button onClick={() => navigate("/todos")}>Todos</button>
+      {/* <br /><br/> */}
+ 
+      <button onClick={() => navigate("/users")}>Users</button>
+      {/* <br /><br/> */}
+
+      {/* <button onClick={() => navigate("/Albums")}>Albums</button> */}
+    </div>
+  );
+}
+ 
+export default Home;
+
+// import{useNavigate} from 'react-router-dom'
+
+// function Home(){
+
+//     const navigate = useNavigate();
+
+//     return(
+//         <div style={{padding : "20px"}}>
+//             <h2>Home</h2>
+
+//             <button  onclick={()=> navigate("/posts")}>posts </button>
+//             <br/><br/>
+
+//             <button onclick={()=> navigate("/todos")}>todos </button>
+//             <br/><br/>
+
+//             <button onclick={()=> navigate("/Users")}> Users </button>
+//             <br/><br/>
+
+//         </div>
+//     );
+// }
+
+// export default Home;
